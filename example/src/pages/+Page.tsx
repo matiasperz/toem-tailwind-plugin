3f800cc2eafe53549a2e4affdcd28dbf385f4249
Matias Perez <matiasperezpc@hotmail.com>
2024-07-17 21:35:49 -0300

    upd

--- example/src/pages/+Page.tsx
+++ example/src/pages/+Page.tsx
@@ -1,29 +1,36 @@
 import { Fragment, useCallback, useMemo, useState } from "react";
 import clsx from "clsx";
-import { CopyIcon, CheckIcon, ReaderIcon } from '@radix-ui/react-icons'
+import {
+  CopyIcon,
+  CheckIcon,
+  ReaderIcon,
+  ChevronDownIcon,
+} from "@radix-ui/react-icons";
+import * as Select from "@radix-ui/react-select";
 
 import "./App.css";
-import s from './Components.module.css';
-import TailwindLogo from '../assets/tailwind.svg'
+import s from "./Components.module.css";
+import TailwindLogo from "../assets/tailwind.svg";
 import { useData } from "vike-react/useData";
 import { Readme } from "./Readme";
 
-export { Page }
+export { Page };
 
 const command = {
-  npm: 'npm install toem-tailwind-plugin',
-  pnpm: 'pnpm install toem-tailwind-plugin',
-  yarn: 'yarn add toem-tailwind-plugin',
-}
+  npm: "npm install toem-tailwind-plugin",
+  pnpm: "pnpm install toem-tailwind-plugin",
+  yarn: "yarn add toem-tailwind-plugin",
+};
 
-const pkgManagers = Object.keys(command) as ('npm' | 'pnpm' | 'yarn')[];
+const pkgManagers = Object.keys(command) as ("npm" | "pnpm" | "yarn")[];
 
-type Context = { readme: string }
+type Context = { readme: string };
 
 function Page() {
-  const ctx = useData<Context>()
-  const [activeExample, setActiveExample] = useState('readme')
-  const [pkgManager, setPkgManager] = useState<typeof pkgManagers[number]>('npm');
+  const ctx = useData<Context>();
+  const [activeExample, setActiveExample] = useState("readme");
+  const [pkgManager, setPkgManager] =
+    useState<(typeof pkgManagers)[number]>("npm");
   const [isCopied, setIsCopied] = useState(0);
 
   const copyValue = useMemo(() => {
@@ -46,72 +53,130 @@ function Page() {
             <span className="inline-flex items-center justify-center mr-2 border rounded-full bg-white/5 size-10 border-zinc-900">
               <img src={TailwindLogo} className="w-6" />
             </span>
-            <code className="inline-block">to-em</code> plugin</p>
+            <code className="inline-block">to-em</code> plugin
+          </p>
         </div>
         <ul className="flex flex-col px-4 py-3 gap-y-3">
-          {[{
-            key: 'readme',
-            icon: <ReaderIcon className="inline-block h-[1em]" />,
-            label: 'Introduction',
-          }, {
-            key: 'quotes',
-            label: 'Scalable quote',
-          }, {
-            key: 'viewport',
-            label: 'Fluid text',
-          }].map(({ key, label, icon }) => {
+          {[
+            {
+              key: "readme",
+              icon: <ReaderIcon className="inline-block h-[1em]" />,
+              label: "Introduction",
+            },
+            {
+              key: "quotes",
+              label: "Scalable quote",
+            },
+            {
+              key: "viewport",
+              label: "Fluid text",
+            },
+          ].map(({ key, label, icon }) => {
             return (
               <Fragment key={key}>
                 <li>
-                  <button onClick={() => setActiveExample(key)} className={clsx(s['sidebar-link'], { [s['active']]: key === activeExample })}>
+                  <button
+                    onClick={() => setActiveExample(key)}
+                    className={clsx(s["sidebar-link"], {
+                      [s["active"]]: key === activeExample,
+                    })}
+                  >
                     {icon} {label}
                   </button>
                 </li>
-                {
-                  key === 'readme' ? (
-                    <li className="pb-2 pl-2 mt-2 border-b border-zinc-900">
-                      <span className="text-sm leading-none">EXAMPLES</span>
-                    </li>
-                  ) : <></>
-                }
+                {key === "readme" ? (
+                  <li className="pb-2 pl-2 mt-2 border-b border-zinc-900">
+                    <span className="text-sm leading-none">EXAMPLES</span>
+                  </li>
+                ) : (
+                  <></>
+                )}
               </Fragment>
-            )
+            );
           })}
         </ul>
 
         <div className="mt-auto text-sm border-t border-zinc-900">
           <div className="flex justify-between px-3 py-2">
-            <select onChange={(e) => {
-              setPkgManager(e.target.value as 'npm' | 'pnpm' | 'yarn');
-            }} className="border-white/5 border px-em-[12/16] py-em-[4/16] rounded-em-[8/16]" name="" id="">
-              <option value="npm">npm</option>
-              <option value="pnpm">pnpm</option>
-              <option value="yarn">yarn</option>
-            </select>
+            <Select.Root
+              value={pkgManager}
+              onValueChange={(v) => {
+                setPkgManager(v as "npm" | "pnpm" | "yarn");
+              }}
+            >
+              <Select.Trigger className="flex gap-x-em-[8/16] items-center border-white/5 bg-zinc-900 border px-em-[12/16] py-em-[4/16] rounded-em-[8/16]">
+                <Select.Value asChild>
+                  <span className="pointer-events-none text-left inline-block min-w-[4.5ch]">
+                    {pkgManager}
+                  </span>
+                </Select.Value>
+                <Select.Icon className="">
+                  <ChevronDownIcon />
+                </Select.Icon>
+              </Select.Trigger>
 
-            <button disabled={Boolean(isCopied)} onClick={onCopy} className={clsx(s["copy-button"], {
-              [s['copy-button-copied']]: isCopied
-            })}>
+              <Select.Portal>
+                <Select.Content className="text-sm" position="popper">
+                  <Select.Viewport
+                    style={{
+                      width: "var(--radix-popper-anchor-width)",
+                    }}
+                    className="my-2 rounded-em-[8/16] border border-white/5 bg-zinc-900"
+                  >
+                    {pkgManagers.map((manager) => (
+                      <Select.Item
+                        className="flex gap-x-em-[8/16] items-center justify-between px-em-[12/16] py-em-[4/16] transition-colors hover:bg-white/5 outline-none cursor-pointer"
+                        key={manager}
+                        value={manager}
+                      >
+                        <Select.ItemText>{manager}</Select.ItemText>
+                        <Select.ItemIndicator>
+                          <CheckIcon />
+                        </Select.ItemIndicator>
+                      </Select.Item>
+                    ))}
+                  </Select.Viewport>
+                </Select.Content>
+              </Select.Portal>
+            </Select.Root>
+
+            <button
+              disabled={Boolean(isCopied)}
+              onClick={onCopy}
+              className={clsx(s["copy-button"], {
+                [s["copy-button-copied"]]: isCopied,
+              })}
+            >
               <CopyIcon width={16} height={16} />
               <CheckIcon width={20} height={20} />
             </button>
           </div>
 
-          <button disabled={Boolean(isCopied)} onClick={onCopy} className={clsx("relative overflow-hidden inline-flex w-full px-4 py-3 border-t border-white/5 bg-zinc-900 items-bottom")}>
-            <div className={clsx(s['install-command-bg'], {
-              [s['install-command-bg-copied']]: isCopied
-            })} />
+          <button
+            disabled={Boolean(isCopied)}
+            onClick={onCopy}
+            className={clsx(
+              "relative overflow-hidden inline-flex w-full px-4 py-3 border-t border-white/5 bg-zinc-900 items-bottom"
+            )}
+          >
+            <div
+              className={clsx(s["install-command-bg"], {
+                [s["install-command-bg-copied"]]: isCopied,
+              })}
+            />
             <code className="inline-block">
-              {'>'} {command[pkgManager]}
+              {">"} {command[pkgManager]}
             </code>
-            <span className="h-em-[20/16] bg-current align-middle inline-block w-em-[10/16] animate-caret ml-em-[6/16]">{' '}</span>
+            <span className="h-em-[20/16] bg-current align-middle inline-block w-em-[10/16] animate-caret ml-em-[6/16]">
+              {" "}
+            </span>
           </button>
         </div>
       </div>
 
       <div className="flex flex-col items-center justify-center p-10 leading-none gridbg gap-y-6">
-        {
-          activeExample === 'quotes' ? (<div className="flex flex-col gap-y-6">
+        {activeExample === "quotes" ? (
+          <div className="flex flex-col gap-y-6">
             <div className="mx-auto space-y-4 text-xs max-w-max">
               <p className="px-em-[16/16]">Extra Small</p>
               <Quote />
@@ -128,49 +193,64 @@ function Page() {
               <p className="px-em-[16/16]">Large</p>
               <Quote />
             </div>
-          </div>) : <></>
-        }
+          </div>
+        ) : (
+          <></>
+        )}
 
-        {
-          activeExample === 'viewport' ? (
-            <div className={s['viewport']}>
-              <div className="text-em-[22/16] mx-auto max-w-em-[780/22] !space-y-em-[24/22]">
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </div>
+        {activeExample === "viewport" ? (
+          <div className={s["viewport"]}>
+            <div className="text-em-[22/16] mx-auto max-w-em-[780/22] !space-y-em-[24/22]">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
             </div>
-          ) : (<></>)
-        }
+          </div>
+        ) : (
+          <></>
+        )}
 
-        {
-          activeExample === 'readme' ? (
-            <Readme content={ctx.readme} />
-          ) : <></>
-        }
+        {activeExample === "readme" ? <Readme content={ctx.readme} /> : <></>}
       </div>
-
     </div>
   );
 }
@@ -180,24 +260,25 @@ const Quote = () => {
     <>
       {/* A tailwind quote component quoting steve jobs */}
       <div className="flex bg-black flex-col items-start justify-center p-em-[24/16] mx-auto space-y-4 border max-w-max rounded-xl border-zinc-900">
-        <div className="flex flex-col items-center space-y-em-[16/16] md:flex-row md:space-y-em-[0/1] md:space-x-em-[16/16]">
+        <div className="flex flex-col items-center text-center xl:text-left space-y-em-[16/16] xl:flex-row xl:space-y-em-[0/1] xl:space-x-em-[16/16]">
           <img
-            src="https://dummyimage.com/120x120/fff/000"
+            src="/steve-jobs.png"
             alt="Steve Jobs"
             width={120}
             height={120}
             className="w-em-[64/16] rounded-full"
           />
           <div>
-            <blockquote className="text-em-[24/16] font-medium leading-tight">
+            <blockquote className="text-em-[24/16] max-xl:max-w-[32ch] text-pretty font-medium leading-tight">
               "The only way to do great work is to love what you do."
             </blockquote>
-            <div className="flex items-center gap-x-em-[8/16]">
-              <div className="font-medium">Steve Jobs</div>
-              <div className="text-gray-500 dark:text-gray-400">
+            <p className="gap-x-em-[8/16] mt-em-[8/16]">
+              <span className="font-medium">Steve Jobs</span>
+              &nbsp;
+              <span className="text-gray-500 dark:text-gray-400">
                 Co-founder, Apple
-              </div>
-            </div>
+              </span>
+            </p>
           </div>
         </div>
       </div>
--- example/src/pages/Readme.tsx
+++ example/src/pages/Readme.tsx
@@ -1,19 +1,18 @@
-import Markdown from "react-markdown"
-import rehypeRaw from 'rehype-raw'
-import { ReaderIcon } from '@radix-ui/react-icons'
-import clsx from "clsx"
+import Markdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import { ReaderIcon } from "@radix-ui/react-icons";
+import clsx from "clsx";
 
-export { Readme }
+export { Readme };
 
-const proseBase = 'prose prose-invert prose-zinc prose-blue'
-const proseCode= 'prose-pre:bg-zinc-900 prose-pre:border prose-pre:border-white/5'
-const proseHeadings = 'prose-headings:font-semibold'
+const proseBase = "prose prose-invert prose-zinc prose-blue";
+const proseCode =
+  "prose-pre:bg-zinc-900 prose-pre:border prose-pre:border-white/5";
+const proseHeadings = "prose-headings:font-semibold prose-headings:border-b prose-headings:border-zinc-900 prose-headings:pb-2";
 
-const Readme = ({content}: {
-  content: string
-}) => {
+const Readme = ({ content }: { content: string }) => {
   return (
-    <div className="flex max-w-[1200px] flex-col w-full h-full bg-black border rounded-xl border-zinc-900">
+    <div className="flex max-w-[1200px] flex-col w-full h-full bg-black border rounded-xl overflow-hidden border-zinc-900">
       <header className="border-b border-zinc-900">
         <div className="inline-block px-4 py-3 border-r border-zinc-900">
           <h1 className="text-sm font-medium flex items-center gap-x-em-[8/16]">
@@ -21,14 +20,36 @@ const Readme = ({content}: {
           </h1>
         </div>
       </header>
-      <div className={clsx("w-full px-12 py-16 !max-w-none", proseBase, proseCode, proseHeadings)}>
-        <Markdown rehypePlugins={[rehypeRaw]}>
-          {content}
-        </Markdown>
+      <div
+        className={clsx(
+          "w-full px-12 py-16 !max-w-none",
+          proseBase,
+          proseCode,
+          proseHeadings
+        )}
+      >
+        <Markdown rehypePlugins={[rehypeRaw]}>{content}</Markdown>
       </div>
-      <footer className="px-4 py-3 mt-auto text-sm border-t border-zinc-900">
-        Made with <span className="inline-block text-[red] animate-heartbeat">❤️</span> by <a target="_blank" rel="noopener" href="https://matiasperez.dev">matiasperz</a>
+      <footer className="flex justify-between mt-auto text-sm border-t border-zinc-900">
+        <div className="px-4 py-3">
+          Made with{" "}
+          <span className="inline-block text-[red] animate-heartbeat">❤️</span>{" "}
+          by{" "}
+          <a target="_blank" rel="noopener" href="https://matiasperez.dev">
+            matiasperz
+          </a>
+        </div>
+        <a href="https://github.com/matiasperz/toem-tailwind-plugin" rel="noopener" className="inline-flex items-center h-full px-4 transition-colors border-l border-zinc-900 hover:bg-white/5">
+          <svg width={20} height={20} viewBox="0 0 98 96"  xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+              fill="white"
+            />
+          </svg>
+        </a>
       </footer>
     </div>
-  )
-}
+  );
+};
