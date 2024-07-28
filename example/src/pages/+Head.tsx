d1ac2e920552e1f500ff59589d5e40e522a6acd9
Matias Perez <matiasperezpc@hotmail.com>
2024-07-28 19:06:55 -0300

    update twitter meta

--- example/src/pages/+Head.tsx
+++ example/src/pages/+Head.tsx
@@ -9,6 +9,14 @@ const Head = () => {
         content="A Tailwind plugin for dynamically calculate em values."
       />
       <meta property="og:image" content="/OG.jpg" />
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="toem Tailwind Plugin" />
+      <meta
+        name="twitter:description"
+        content="A Tailwind plugin for dynamically calculate em values."
+      />
+      <meta name="twitter:image" content="/OG.jpg" />
       <title>toem Tailwind Plugin</title>
     </>
   );
