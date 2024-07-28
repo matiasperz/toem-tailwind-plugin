884f4f3d6de16811da93e0853b2f7d4f3f7a6fcf
Matias Perez <matiasperezpc@hotmail.com>
2024-07-28 19:35:49 -0300

    update

--- example/src/pages/+Head.tsx
+++ example/src/pages/+Head.tsx
@@ -1,23 +1,31 @@
 export { Head };
 
+import og from '../../public/OG.jpg'
+
+const metadata = {
+  title: 'toem Tailwind Plugin',
+  description: 'A Tailwind plugin for dynamically calculate em values.',
+  image: og,
+}
+
 const Head = () => {
   return (
     <>
-      <meta property="og:title" content="toem Tailwind Plugin" />
+      <meta property="og:title" content={metadata.title} />
       <meta
         property="og:description"
-        content="A Tailwind plugin for dynamically calculate em values."
+        content={metadata.description}
       />
-      <meta property="og:image" content="/OG.jpg" />
+      <meta property="og:image" content={metadata.image} />
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="toem Tailwind Plugin" />
+      <meta name="twitter:title" content={metadata.title} />
       <meta
         name="twitter:description"
-        content="A Tailwind plugin for dynamically calculate em values."
+        content={metadata.description}
       />
-      <meta name="twitter:image" content="/OG.jpg" />
-      <title>toem Tailwind Plugin</title>
+      <meta name="twitter:image" content={metadata.image} />
+      <title>{metadata.title}</title>
     </>
   );
 };
